de976ceaa6eb9e741585b0ff221a7b644bc0e3bf
biosyshack <david.lauenstein@gmail.com>
2018-04-26 11:19:17 +0200

    Update comments and annotation

--- microarray-processing/retrieveGEO.R
+++ microarray-processing/retrieveGEO.R
@@ -1,18 +1,28 @@
+#######################################################
+# Filename: retrieveGEO.R
+# Created: David Lauenstein
+# Change history:
+# 26.04.2018 / David Lauenstein
+#######################################################
+# Retrieval and export of annotated GEO datasets
+#######################################################
+
 library(GEOquery)
 
 gse = getGEO("GSE22862", GSEMatrix=TRUE, AnnotGPL=F)
-
-gse = gse[[1]] # get just the first element in list
+gse = gse[[1]] # get first element in list
 
 columnnames = as.matrix(fData(gse)) # check column names for gene symbols
-
 symbols = as.matrix(fData(gse))[,'gene_assignment']
 
-expr_mat = exprs(gse)        # get the expression matrix
 
-rownames(expr_mat) = symbols # map gene symbols to rows
+# get the expression matrix
+expr_mat = exprs(gse) 
 
-# export annotated expression matrix
+# map gene symbols to rows
+rownames(expr_mat) = symbols 
+
+# export annotated expression matrix and numeric expression matrix
 write.table(as.matrix(expr_mat), '~/GSE22862_annotate.txt',
             sep = '\t', quote = F, row.names = T, col.names = T)
 write.table(as.matrix(expr_mat), '~/GSE22862_num.txt',
