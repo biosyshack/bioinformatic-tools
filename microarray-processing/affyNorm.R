133be4fff2dab4e465628e65dd72844038292491
biosyshack <david.lauenstein@gmail.com>
2018-04-26 11:23:39 +0200

    update comments and annotation

--- microarray-processing/affyNorm.R
+++ microarray-processing/affyNorm.R
@@ -1,38 +1,43 @@
+#######################################################
+# Filename: affyNorm.R
+# Created: David Lauenstein
+# Change history:
+# 26.04.2018 / David Lauenstein
+#######################################################
+# Retrieval and normalization of Affymetrix datasets (GEO) using robust multi-array average (RMA)
+#######################################################
+
 library(GEOquery)
 library(affy)
 
-# Set working directory for download
+# set working directory for download
 setwd("~/GSE22862")
 
+# download dataset
 getGEOSuppFiles("GSE22862")
 
 setwd("~/GSE22862/GSE22862")
 
+# unpack dataset
 untar("GSE22862_RAW.tar", exdir = "data")
 cels = list.files("~/GSE22862/GSE22862/data", pattern = "CEL")
 sapply(paste("data", cels[], sep = "/"), gunzip)
 
 cels = list.files("~/GEO/GSE22862/GSE22862/data", full=T)
 
-# Set working directory for normalization
+# set working directory for normalization
 setwd("~/GSE22862/GSE22862/data")
 
-raw.data=ReadAffy(verbose=TRUE, filenames=cels, cdfname="hgu133plus2") #From bioconductor, define cdf
+raw.data=ReadAffy(verbose=TRUE, filenames=cels, cdfname="hgu133plus2") #From bioconductor, cdf has to be installed
 
 # perform RMA normalization (log2)
 data.rma.norm = rma(raw.data)
 
-# Get the expression estimates for each array
+# get the expression estimates for each array
 rma = exprs(data.rma.norm)
 
-# Take a look at the result (first 5 rows and 5 columes)
+# take a look at the result (first 5 rows and 5 columes)
 rma[1:5, 1:5]
 
+# export normalized expression matrix
 write.table(rma, file = "rma.txt", quote = FALSE, sep = "\t")
-
-tt = cbind(row.names(rma), rma)
-colnames(tt) = c("ProbID", sub(".cel", "", colnames(rma), ignore.case = TRUE))
-rownames(tt) = NULL
-tt[1:5, 1:5]
-
-write.table(tt, file = "rma_matrix.txt", quote = FALSE, sep = "\t")
